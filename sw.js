551137b1b0977d876b1030f7522310b51a9d2473
theedrradical <camerondburr@gmail.com>
2026-02-20 11:06:23 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,13 @@ const urlsToCache = [
   './manifest.json',
   './images/empty8.png',
   './images/personal-artwork-3 copy.jpg',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
   './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
   './icons/icon-512x512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css'
 ];
